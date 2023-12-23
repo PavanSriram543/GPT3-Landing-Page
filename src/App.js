7dd20c32f5f1842ba3d2baa0f1b81042ce0c5006
Pavan Kumar <pavankumar@Pavans-MacBook-Air.local>
2023-12-23 17:21:21 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {
+  Footer,
+  Header,
+  Blog,
+  Possibility,
+  Fatures,
+  WhatisGPT3,
+} from "./containers/index";
+import { Brand, CTA } from "./components/index";
+import NavBar from "./components/navbar/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>GPT-#</h1>
+      <div className="gradient__bg">
+        <NavBar />
+        <Header />
+      </div>
+      <div>
+        <Brand />
+        <WhatisGPT3 />
+        <Fatures />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/containers/whatisGPT3/WhatisGPT3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./whatisGPT3.css";
+
+function WhatisGPT3() {
+  return (
+    <div>
+      <h1>WhatisGPT3</h1>
+    </div>
+  );
+}
+
+export default WhatisGPT3;
