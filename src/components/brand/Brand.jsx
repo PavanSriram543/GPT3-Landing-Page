7d65dc62d492429cd138042177f589a92acb861c
Pavan Kumar <pavankumar@Pavans-MacBook-Air.local>
2023-12-23 17:53:34 +0530

    brand artical and header added

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+
 import "./brand.css";
 
-function Brand() {
-  return (
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
     <div>
-      <h1>Brand</h1>
+      <img src={google} alt="google" />
     </div>
-  );
-}
+    <div>
+      <img src={slack} alt="slack" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassiam" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify" />
+    </div>
+  </div>
+);
 
 export default Brand;
---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../asets/google.png";
+import slack from "../../asets/slack.png";
+import atlassian from "../../asets/atlassian.png";
+import dropbox from "../../asets/dropbox.png";
+import shopify from "../../asets/shopify.png";
+
+export { google, slack, atlassian, dropbox, shopify };
