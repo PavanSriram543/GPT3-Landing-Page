66ba8ad2cfd48b23d387da5017e878efa4a560e1
Pavan Kumar <pavankumar@Pavans-MacBook-Air.local>
2023-12-23 18:00:53 +0530

    final push

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,7 @@
+import blog01 from "../../asets/blog01.png";
+import blog02 from "../../asets/blog02.png";
+import blog03 from "../../asets/blog03.png";
+import blog04 from "../../asets/blog04.png";
+import blog05 from "../../asets/blog05.png";
+
+export { blog01, blog02, blog03, blog04, blog05 };
